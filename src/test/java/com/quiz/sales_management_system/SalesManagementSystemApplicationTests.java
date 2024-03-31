c1231ddd93c7ee021832052c033d4f43c76176c8
Omar Hatem <omar.kortam@cic.ae>
2024-03-31 21:42:55 +0200

    -Added Project on GitHub.

---
+++ src/test/java/com/quiz/sales_management_system/SalesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.sales_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
